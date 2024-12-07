1ba514c7bc0726b8ef0017bf54805aa26ba92a4e
janisfs <janisfs@mail.ru>
2024-12-08 00:01:29 +0300

    add new code, add new pages

--- flower_delivery/my_flwr_dlvr/views.py
+++ flower_delivery/my_flwr_dlvr/views.py
@@ -3,21 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the my_flwr_dlvr index.")
-   # return render(request, 'index.html')
+    # return HttpResponse("Hello, world. You're at the my_flwr_dlvr index.")
+    return render(request, 'my_flwr_dlvr/index.html')
 
 
 def login_view(request):
-    return HttpResponse("Login page.")
+    # return HttpResponse("Login page.")
+    return render(request, 'my_flwr_dlvr/login.html')
 
 
 def register_view(request):
-    return HttpResponse("Register page.")
+    # return HttpResponse("Register page.")
+    return render(request, 'my_flwr_dlvr/register.html')
 
 
 def logout_view(request):
-    return HttpResponse("Logout page.")
+    # return HttpResponse("Logout page.")
+    return render(request, 'my_flwr_dlvr/logout.html')
 
 
 def order_view(request):
-    return HttpResponse("Order page.")
+    # return HttpResponse("Order page.")
+    return render(request, 'my_flwr_dlvr/order.html')
